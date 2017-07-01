d9bcb699fb44ee0444aa528d33f4bda43f1ce678
MrJo <MrJo@MRJO>
2017-07-01 14:27:56 +0700

    init

---
+++ AxonCareerDay2017/PersonalFinanceManagement/Entities/Arise.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PersonalFinanceManagement.Entities
+{
+    public class Arise
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public bool Type { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal Money { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ AxonCareerDay2017/PersonalFinanceManagement/Entities/UserInfomation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PersonalFinanceManagement.Entities
+{
+    public class UserInfomation
+    {
+        public int UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public bool Gender { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public decimal DailyLevel { get; set; }
+
+        public decimal WeeklyLevel { get; set; }
+        
+        public decimal MonthlyLevel { get; set; }
+    }
+}
